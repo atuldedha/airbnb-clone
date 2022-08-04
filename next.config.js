47d2edaf4626ba6b85e914ef95ea0cac7dadb822
atuldedha <auldedha20@gmail.com>
2022-08-04 06:21:34 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    images: {
+
+        domains: ["links.papareact.com"],
+    },
+    //Only put public keys this way
+    env: {
+        mapbox_key: 'pk.eyJ1IjoiYXR1bGRlZGhhIiwiYSI6ImNrdGQzYmNiMDJieHQyb2pwYnQzeXRmcG8ifQ.53NRHI-1R2Pfal7di3xtqQ'
+    }
+}
